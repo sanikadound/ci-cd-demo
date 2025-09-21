2a9879f8897a4cb494e88a4c6b534a1ed2cd5686
sanikadound <sanikadound@gmail.com>
2025-09-21 15:33:30 +0000

    Added app.py and deploy.sh for CI/CD demo

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "----- Build Step -----"
+echo "Building the application..."
+
+echo "----- Test Step -----"
+python3 app.py
+
+echo "----- Deploy Step -----"
+echo "Deploying application..."
